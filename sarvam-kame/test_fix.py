56be4797d2ca7c8fa1e8462070e039e3b160ddcb
Kanan Kotwani <kanankotwani28@gmail.com>
2026-05-28 13:30:41 +0530

    rermoved the testing scripts

--- sarvam-kame/test_fix.py
+++
@@ -1,7 +0,0 @@
-import re
-text = "<think>\nOkay, the user said hello.\n</think>\n\nHello Kanan! Nice to meet you."
-clean = re.sub(r'<think>.*?
-</think>
-
-', '', text, flags=re.DOTALL).strip()
-print('CLEAN:', clean)
